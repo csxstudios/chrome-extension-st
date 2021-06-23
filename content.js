729ee42908b499dfa9b3b7ff15a7a9b07a6b71ad
csxstudios <csxstudios@gmail.com>
2021-06-23 19:02:47 -0400

    remove unused

--- content.js
+++ content.js
@@ -1,8 +1,3 @@
-// var images = document.getElementsByTagName('img');
-// for (var i = 0, l = images.length; i < l; i++) {
-//   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
-// }
-
 $(function() {
   //document ready
 });
@@ -10,7 +5,6 @@ $(function() {
 $(window).on("load", function() {
   var tickers = ["TSLA", "FUV", "GLW", "PLTR", "MSFT", "CHEK"]
   var cycle = (function() {
-    console.log("start");
     setInterval(function() {
       var i = getRandomInt(tickers.length);
       var target = "a[href='https://stocktwits.com/symbol/" + tickers[i] + "']";
@@ -54,49 +48,3 @@ $(window).on("load", function() {
         });
 	});
 });
-
-// var targets ={
-//     [$('h5').parent().attr("class")] : 'overflow :visible', 
-//     [$('h5').parent().parent().attr("class")] : 'display: block',
-//     [$("span").parent().attr("class")] : 'font-size: 1rem'
-// };
-
-// var targetStyles =[
-//     $('h5').parent().attr("class"), 
-//     $('h5').parent().parent().attr("class"),
-//     $("span").parent().attr("class")
-// ];
-
-// function cssNode(targetClass, styles) {
-//     return "." + targetClass.replaceAll(" ", ".") + " {" + styles + "} \r\n";
-// }
-
-// var newStyles = "";
-// for(var key in targets) {
-//     newStyles = newStyles + cssNode(key, targets[key]);
-// };
-  
-// var dupeTest = "";
-// $('div[draggable="true"]').each( function() {
-//     var tileClass = $(this).attr("class");
-//     var tileColor = $(this).find("span").parent().css("color");
-//     if (tileColor) {
-//         tileColor = tileColor.replace("rgb", "rgba");
-//         tileColor = tileColor.slice(0, tileColor.length-1) + ", 0.3" + tileColor.slice(tileColor.length-1, tileColor.length);
-//         if (tileClass != dupeTest) {
-//             dupeTest = tileClass;
-//             newStyles = newStyles + cssNode(tileClass, "background-color: " + tileColor);
-//         }
-//         //$(this).css({"background-color":tileColor});
-//     } else {
-//         if (tileClass != dupeTest) {
-//             dupeTest = tileClass;
-//             newStyles = newStyles + cssNode(tileClass, "background-color: rgba(16, 24, 255, 0.3)");
-//         }
-//         //$(this).css({"background-color":"rgba(16, 24, 255, 0.3)"});
-//     }
-// });
-
-// $("head").append('<style type="text/css"></style>');
-// var new_stylesheet = $("head").children(':last');
-// new_stylesheet.text(newStyles);
